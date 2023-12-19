107fd93e51c51307f610d37886d0e50b99a6fa5e
Venkatanarasimha Reddy Burri <venkat@s-MacBook-Pro.lan>
2023-12-19 13:40:10 -0500

    firebase config

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,7 +8,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //add config from firebase
+  apiKey: 'AIzaSyAswFgml9JoujrRSrR2ObTlIlu9yAR1uGY',
+  authDomain: 'ivory-events-management.firebaseapp.com',
+  projectId: 'ivory-events-management',
+  storageBucket: 'ivory-events-management.appspot.com',
+  messagingSenderId: '1082854548083',
+  appId: '1:1082854548083:web:bef68edbd40f8172c70006',
+  measurementId: 'G-YB7XDPTNW7'
 };
 
 // Initialize Firebase
